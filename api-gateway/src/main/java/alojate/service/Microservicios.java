2eb2a2d35506e5c7da6eeff9d93777c568bd75e0
manugomezp <mgomezpereyra@fraba.utn.edu.ar>
2026-01-21 18:46:45 -0300

    gateway consulta a service registry ante una solicitud del cliente

--- api-gateway/src/main/java/alojate/service/Microservicios.java
+++ api-gateway/src/main/java/alojate/service/Microservicios.java
@@ -15,53 +15,53 @@ public class Microservicios {
         this.builder = builder;
     }
 
-    @Bean
-    public RouteLocator publicaciones() {
-        return builder.routes()
-                .route("publicaciones_route", r -> r
-                        .path("/alojate/publicaciones/**")
-                        .filters(f -> f
-                                .rewritePath(
-                                        "/alojate/publicaciones/(?<segment>.*)",
-                                        "/api/publicaciones${segment}"
-                                )
-                        )
-                        .uri("http://localhost:8090")
-                )
-                .build();
-    }
-
-    @Bean
-    public RouteLocator reservas() {
-        return builder.routes()
-                .route("reservas_route", r -> r
-                        .path("/alojate/reservas/**")
-                        .filters(f -> f
-                                .rewritePath(
-                                        "/alojate/reservas/(?<segment>.*)",
-                                        "/api/reservas${segment}"
-                                )
-                        )
-                        .uri("http://localhost:9000")
-                )
-                .build();
-    }
-
-    @Bean
-    public RouteLocator usuarios() {
-        return builder.routes()
-                .route("usuarios_route", r -> r
-                        .path("/alojate/usuarios/**")
-                        .filters(f -> f
-                                .rewritePath(
-                                        "/alojate/usuarios/(?<segment>.*)",
-                                        "/api/usuarios${segment}"
-                                )
-                        )
-                        .uri("http://localhost:9020")
-                )
-                .build();
-    }
+//    @Bean
+//    public RouteLocator publicaciones() {
+//        return builder.routes()
+//                .route("publicaciones_route", r -> r
+//                        .path("/alojate/publicaciones/**")
+//                        .filters(f -> f
+//                                .rewritePath(
+//                                        "/alojate/publicaciones/(?<segment>.*)",
+//                                        "/api/publicaciones${segment}"
+//                                )
+//                        )
+//                        .uri("http://localhost:8090")
+//                )
+//                .build();
+//    }
+//
+//    @Bean
+//    public RouteLocator reservas() {
+//        return builder.routes()
+//                .route("reservas_route", r -> r
+//                        .path("/alojate/reservas/**")
+//                        .filters(f -> f
+//                                .rewritePath(
+//                                        "/alojate/reservas/(?<segment>.*)",
+//                                        "/api/reservas${segment}"
+//                                )
+//                        )
+//                        .uri("http://localhost:9000")
+//                )
+//                .build();
+//    }
+//
+//    @Bean
+//    public RouteLocator usuarios() {
+//        return builder.routes()
+//                .route("usuarios_route", r -> r
+//                        .path("/alojate/usuarios/**")
+//                        .filters(f -> f
+//                                .rewritePath(
+//                                        "/alojate/usuarios/(?<segment>.*)",
+//                                        "/api/usuarios${segment}"
+//                                )
+//                        )
+//                        .uri("http://localhost:9020")
+//                )
+//                .build();
+//    }
 
 
 
